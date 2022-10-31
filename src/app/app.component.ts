212439858d9a29830ee83dc63f0649debba941b7
steerven <lucasjoelcampodonico@gmail.com>
2022-10-31 10:26:28 -0300

    A

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,135 +1,200 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
-import { Chart, ChartDataSets, ChartData, ChartOptions } from 'chart.js';
+import { Component, ViewChild, ElementRef, OnInit } from "@angular/core";
+import { Chart, ChartDataSets, ChartData, ChartOptions } from "chart.js";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-
-  @ViewChild('grafica', { static: true }) graficaREF: ElementRef;
-
-  ngOnInit(){
-
+  chart: Chart;
+  dataLeft: ChartDataSets = [
+    292.67, 292.82, 292.54, 292.54, 292.95, 292.41, 292.68, 292.27, 292.41,
+    292.21, 292.37, 292.5,
+  ];
+  dataRight: ChartDataSets = [
+    21.43, 21.43, 26.78, 16.07, 16.07, 10.71, 10.71, 21.43, 5.36, 10.71, 5.36,
+    10,
+  ];
+  ngOnInit() {
     // CHARTDATASET CONFIGURATION
-    const datasets:ChartDataSets[] = [
+    const datasets: ChartDataSets[] = [
       {
-        label: 'Fan page',
+        label: "Fan page",
         fill: false,
-        data: [292.67, 292.82, 292.54, 292.54, 292.95, 292.41, 292.68, 292.27, 292.41, 292.21, 292.37, 292.50],
-        backgroundColor: '#4C4CD8',
-        borderColor: '#4C4CD8',
-        borderWidth : 1,
-        yAxisID : 'Fan page',
-        pointHoverRadius : 7,
-        pointHoverBackgroundColor : '#4C4CD8',
-        pointHoverBorderColor : '#4c4cd8a1',
-        pointHoverBorderWidth : 10,
+        data: this.dataLeft,
+        backgroundColor: "#4C4CD8",
+        borderColor: "#4C4CD8",
+        borderWidth: 1,
+        yAxisID: "Fan page",
+        pointHoverRadius: 10,
+        pointHoverBackgroundColor: "#2a2a9e", //SE LOGRA EL EFECTO DESEADO MODIFICANDO EL RADIUS Y EL COLOR DEL FONDO DEL PUNTO
+        pointHoverBorderColor: "#4c4cd8a1",
+        pointHoverBorderWidth: 10,
       },
       {
-        label: 'Engaged users',
+        label: "Engaged users",
         fill: false,
-        data: [21.43, 21.43, 26.78, 16.07, 16.07, 10.71, 10.71, 21.43, 5.36, 10.71, 5.36, 10],
-        backgroundColor: '#F8CB1C',
-        borderColor: '#F8CB1C',
-        borderWidth : 1,
-        yAxisID : 'Engaged users',
-        pointHoverRadius : 7,
-        pointHoverBackgroundColor : '#F8CB1C',
-        pointHoverBorderColor : '#f8cb1c85',
-        pointHoverBorderWidth : 10
-      }
-    ]
+        data: this.dataRight,
+        backgroundColor: "#F8CB1C",
+        borderColor: "#F8CB1C",
+        borderWidth: 1,
+        yAxisID: "Engaged users",
+        pointHoverRadius: 10,
+        pointHoverBackgroundColor: "#A88911", //SE LOGRA EL EFECTO DESEADO MODIFICANDO EL RADIUS Y EL COLOR DEL FONDO DEL PUNTO
+        pointHoverBorderColor: "#f8cb1c85",
+        pointHoverBorderWidth: 10,
+      },
+    ];
 
     // CHARTDATA
-    const data : ChartData = {
-      labels : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dic'],
-      datasets
-    }
+    const data: ChartData = {
+      labels: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dic",
+      ],
+      datasets,
+    };
 
     // CHARTOPTIONS
-    const options : ChartOptions = {
-      responsive : false,
-      legend : {
-        display : false
+    const options: ChartOptions = {
+      responsive: true,
+      legend: {
+        display: false,
       },
-      hover : {
-        mode : 'x',
-        intersect : false
+      onHover: (e, items) => {
+        this.pointHover(items); // OBTENEMOS CHART ELEMENT AL PASAR POR LOS PUNTOS
       },
-      scales : {
-        xAxes : [
+      hover: {
+        mode: "nearest", // SOLO HACEMOS HOVER SOBRE EL PUNTO SEÑALADO
+        intersect: true,
+        animationDuration: 0,
+      },
+      scales: {
+        xAxes: [
           {
-            ticks : {
-              fontColor : '#EBEBEB',
-              callback : (value) => {
-                return value
+            ticks: {
+              callback: (value) => {
+                return value;
               },
-              fontSize : 10,
-              fontFamily : 'Work Sans',
-              padding : 6
+              fontColor: "#fff",
+              fontSize: 12,
+              fontFamily: "Work Sans",
+              padding: 6,
             },
-            gridLines : {
-              display : false,
+            gridLines: {
+              display: false,
             },
           },
-
         ],
-        yAxes : [{
-          id : 'Fan page',
-          position : 'left',
-          ticks : {
-            fontColor : '#EBEBEB',
-            padding : 15,
-            fontSize : 10,
-            fontFamily : 'Work Sans',
-            callback : (value:string, index, values) => {
-              const newValue = parseFloat(value).toFixed(2)
-              return newValue + ' K'
-            }
-          },
-          gridLines : {
-            borderDash: [1, 2],
-            color: "#282828",
-            drawBorder: false,
-            tickMarkLength : 0,
-          }
-        },
-        {
-          id : 'Engaged users',
-          position : 'right',
-          ticks : {
-            fontColor : '#B8B7B7',
-            fontSize : 10,
-            padding : 15,
-            fontFamily : 'Work Sans',
-            callback : (value:string, index, values) => {
-              const newValue = parseFloat(value).toFixed(2)
-              return newValue
+        yAxes: [
+          {
+            id: "Fan page",
+            position: "left",
+            ticks: {
+              stepSize:
+                (Math.max(...this.dataLeft) - Math.min(...this.dataLeft)) / 10, // DIVIDIMOS EN PARTES
+              maxTicksLimit: 7, // LIMITAMOS A MAXIMO 7
+              fontColor: "#EBEBEB",
+              padding: 15,
+              fontSize: 10,
+              fontFamily: "Work Sans",
+              callback: (value: string, index, values) => {
+                const newValue = parseFloat(value).toFixed(2);
+                return newValue + " K";
+              },
+            },
+            gridLines: {
+              borderDash: [1, 2],
+              color: "#282828",
+              drawBorder: false,
+              tickMarkLength: 0,
             },
           },
-          gridLines : {
-            drawBorder: false,
-            tickMarkLength : 0,
-            color: 'transparent'
-          }
-        }]
+          {
+            id: "Engaged users",
+            position: "right",
+            ticks: {
+              stepSize:
+                (Math.max(...this.dataRight) - Math.min(...this.dataRight)) /
+                10, // DIVIDIMOS EN PARTES
+              maxTicksLimit: 7, // LIMITAMOS A MAXIMO 7
+              fontColor: "#fff",
+              fontSize: 10,
+              padding: 15,
+              fontFamily: "Work Sans",
+              callback: (value: string, index, values) => {
+                const newValue = parseFloat(value).toFixed(2);
+                return newValue;
+              },
+            },
+            gridLines: {
+              drawBorder: false,
+              tickMarkLength: 0,
+              color: "transparent",
+            },
+          },
+        ],
       },
       tooltips: {
         enabled: false,
-        mode: 'x',
-        intersect: false
-      }
-    }
+      },
+    };
 
     // INIT GRAPH
-    new Chart('grafica', {
-      type: 'line',
+    new Chart("grafica", {
+      type: "line",
       data,
-			options
-    })
-
+      options,
+    });
   }
 
+  pointHover(elements) {
+    console.log(elements);
+    if (elements.length) {
+      let element = elements;
+      if (element[0]) {
+        element = element[0];
+        // GUARDAMOS EL PUNTO EN UNA VARIABLE
+        const yPoint = element._model.y;
+        //LO ASOCIAMOS AL LABELITEM DEL EJE Y
+        const targetPoint = element._yScale._labelItems.reduce(function (
+          previous,
+          current
+        ) {
+          return Math.abs(current.y - yPoint) > Math.abs(previous.y - yPoint)
+            ? previous
+            : current;
+        });
+
+        //ASOCIAMOS EL PUNTO AL LABEL CORRESPONDIENTE DEL EJE X
+        const xPointIndex = element._index;
+        const xLabelItem = element._xScale._labelItems[xPointIndex];
+        const pointColor = element._model.backgroundColor;
+        //CREAMOS LOS NUEVOS LABELS
+        const ctx = element._chart.ctx;
+        const backgroundColor = "#1a1919";
+        ctx.fillStyle = backgroundColor;
+        let width = ctx.measureText(targetPoint.label).width;
+        ctx.fillRect(targetPoint.x - 40, targetPoint.y - 5, width + 5, 15);
+        ctx.fillStyle = pointColor;
+        ctx.fillText(targetPoint.label, targetPoint.x - 40, targetPoint.y + 5);
+        width = ctx.measureText(xLabelItem.label).width;
+        ctx.fillStyle = backgroundColor;
+        ctx.fillRect(xLabelItem.x - 10, xLabelItem.y, width + 5, 15);
+        ctx.fillStyle = pointColor;
+        ctx.fillText(xLabelItem.label, xLabelItem.x - 7, xLabelItem.y + 8);
+      }
+    }
+  }
 }
